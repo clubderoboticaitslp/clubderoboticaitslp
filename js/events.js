fb3bd6dd40446f227b6e2a5d994c36fa9e91dde5
Unknown <eleilson.jebt@gmail.com>
2017-09-18 01:07:31 -0500

    modal

--- js/events.js
+++ js/events.js
@@ -1,10 +1,9 @@
 $(document).ready( function () {
-    $(".modal").modal();
     firebase.database().ref('/form/event_details/').once('value').then(function(snapshot){
         var x=0;
         var btncolor;
         var pathname = window.location.pathname;
-        (pathname=="/Robotik/Eventos")?btncolor='red darken-4':btncolor='teal';
+        (pathname=="/Eventos")?btncolor='red darken-4':btncolor='teal';
         snapshot.forEach(function(childSnapshot) {
             var key = childSnapshot.key;
             $("#panel_eventos").append('<div class="card-panel grey lighten-5 z-depth-1 row"><div class="col s12 m4 l2 center">'+childSnapshot.val().org+'</div><div class="col s12 m8 l7 center">'+childSnapshot.val().cuerpo+'</div><div class="col s12 m12 l3 center"><a class="waves-effect waves-light btn-large '+btncolor+'" href="#modal'+x+'">¡Saber más!</a></div></div> <div id="modal'+x+'" class="modal grey darken-3 modal-fixed-footer"><div class="modal-content"><h4 class="grey lighten-5 center modex">'+key+'</h4>'+childSnapshot.val().page+'</div><div class="modal-footer"><a href="#!" class="modal-action modal-close waves-effect waves-grey btn-flat">Cerrar</a></div></div>');
@@ -19,6 +18,7 @@ $(document).ready( function () {
             endingTop: '100%'
         });
     });
+    $('.modal').modal();
 });
 $(window).load(function(){
     $("#preloader div").delay(800).fadeOut();
