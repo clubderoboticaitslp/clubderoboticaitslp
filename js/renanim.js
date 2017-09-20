6da15fa50811039bb9b746fbf6dcfebf4e4d2678
Unknown <eleilson.jebt@gmail.com>
2017-09-20 01:02:37 -0500

    Renanim
    Consulta completa

--- js/renanim.js
+++ js/renanim.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-    var query = firebase.database().ref("/form/event/").orderByKey();
+    var query = firebase.database().ref("/form/licens/").orderByKey();
     query.once("value").then(function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
             var key = childSnapshot.key;
             var childData = childSnapshot.val();
-            $("#r_consulta").append($('<option>', { value: key, text: key}));
-            $("#r_consulta").material_select();
+            $("#x_lic").append($('<option>', { value: key, text: key}));
+            $("#x_lic").material_select();
         });
     });
+    var query = firebase.database().ref("/form/school/").orderByKey();
+    query.once("value").then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();
+            $("#u_school").append($('<option>', { value: key, text: key}));
+            $("#u_school").material_select();
+        });
+    });
+    $("#user_t").material_select();
     $("#r_sender_consultar").click(function(){
-        $("#r_tab1").empty();
-        firebase.database().ref('/users/').once('value').then(function(snapshot){
+        $("#r_tab1").empty(); firebase.database().ref('/users/').once('value').then(function(snapshot){
             var x=0;
             snapshot.forEach(function(childSnapshot) {
                 var key = childSnapshot.key;
@@ -21,8 +30,12 @@ $(document).ready(function() {
                     if(!$("#l_name").val().length==0 && !childSnapshot.val().l_name.toString().includes($("#l_name").val().toString()))con=false;
                     if(!$("#email").val().length==0 && !childSnapshot.val().email.toString().includes($("#email").val().toString()))con=false;
                     if(!$("#u_grade").val().length==0 && !childSnapshot.val().u_grade.toString().includes($("#u_grade").val().toString()))con=false;
-                    alert($('input[name=u_grade_sem_year]:checked').val()+childSnapshot.val().u_grade_sem_year.val()+$("#u_grade_sem_year").val().toString());
-                    if(!$('input[name=u_grade_sem_year]:checked').val() && !childSnapshot.val().u_grade_sem_year.val()==$("#u_grade_sem_year").val().toString()))con=false;
+                    if($( 'input[name=u_grade_sem_year]' ).is(':checked') && !childSnapshot.val().u_grade_sem_year.toString().includes($( 'input[name=u_grade_sem_year]:checked' ).val().toString()))con=false;
+                    if($( 'input[name=u_grade_t]' ).is(':checked') && !childSnapshot.val().u_grade_t.toString().includes($( 'input[name=u_grade_t]:checked' ).val().toString()))con=false; 
+                    if($("#x_lic").val()!="0" && !childSnapshot.val().u_lic.toString().includes($("#x_lic").val()))con=false;
+                    if(!$("#u_mat").val().length==0 && !childSnapshot.val().u_mat.toString().includes($("#u_mat").val().toString()))con=false;
+                    if($("#u_school").val()!="0" && !childSnapshot.val().u_school.toString().includes($("#u_school").val()))con=false;
+                    if($("#user_t").val()!="0" && !childSnapshot.val().user_t.toString().includes($("#user_t").val()))con=false;
                     if(con){
                         snapshot.forEach(function(childSnapshot2) {
                             var childData = childSnapshot2.val();
